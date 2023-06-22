c85d89c798dac539c468205cd924b1a6c2aa13da
Genius-21-11 <119961381+Genius-21-11@users.noreply.github.com>
2023-06-22 17:57:13 +0300

    Add files via upload

---
+++ java_hw4/task1.java
@@ -0,0 +1,23 @@
+// Вывести список на экран в перевернутом виде (без массивов и ArrayList).
+import java.util.Deque;
+import java.util.LinkedList;
+import java.util.Scanner;
+
+public class task1 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Введите количество элементов: ");
+        int n = sc.nextInt();
+        sc.close();
+        Deque<Integer> ourDeque = new LinkedList<>();
+        for (int i = 1; i <= n; i++) {
+            ourDeque.add(i);
+        }
+        System.out.println(ourDeque);
+
+        System.out.println("Перевернутый список");
+        while (ourDeque.size() > 0) {
+            System.out.print(ourDeque.pollLast() + " ");
+        }
+    }
+}
